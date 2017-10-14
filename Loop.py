7aad5de4bdd6e1d498b2388fbfe70c64d47bc350
prthakoordial <32420371+prthakoordial@users.noreply.github.com>
2017-10-14 14:56:43 -0400

    Add files via upload

---
+++ Loop.py
@@ -0,0 +1,12 @@
+number = 0
+counter = 0
+# Loop here
+while counter < 100:
+    number =  number + 1
+    counter = counter + 1
+    print("Counter: {}, number: {} ".format(counter, number))
+
+for i in range(100):
+    print(i)
+
+print(number)
